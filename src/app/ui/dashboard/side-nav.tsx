e9dae3485cb25923715668a4fe0cdd346552a3a1
koolkiddev <koolfacts67@gmail.com>
2024-04-26 18:22:38 -0500

    sidenav changes

--- src/app/ui/dashboard/side-nav.tsx
+++ src/app/ui/dashboard/side-nav.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2 outline outline-2 outline-slate-100 shadow-md">
+    <div className="flex h-full flex-col px-3 py-4 md:px-2 outline outline-1 outline-slate-100">
       {/* title */}
-      <div className="rounded bg-gray-100 mb-4 mr-1 ml-1 shadow">
+      <div className="rounded mb-4 mr-1 ml-1 outline outline-1 outline-slate-100">
         <Link
           className="mb-2 flex h-20 items-end justify-start p-4 md:h-40"
           href="/"
@@ -18,22 +18,22 @@ export default function SideNav() {
       </div> 
 
       {/* nav links */}
-      <div className="flex grow flex-row p-2 mb-4 mr-1 ml-1 justify-between space-x-4 md:flex-col md:space-x-0 md:space-y-4 rounded bg-gray-100 shadow">
-        <div className="hover:bg-gray-200">
+      <div className="flex grow flex-row mb-4 mr-1 ml-1 justify-between space-x-4 md:flex-col md:space-x-0 md:space-y-4">
+        <div className="hover:bg-gray-50 rounded p-2 outline outline-1 outline-slate-100">
           <Link className="w-full font-bold py-2 p-2 mb-auto rounded-xl" href="/dashboard/">Home</Link>
         </div>
-        <div className="hover:bg-gray-200">
+        <div className="hover:bg-gray-50 rounded p-2 outline outline-1 outline-slate-100">
           <Link className="w-full font-bold py-2 p-2 mb-auto rounded" href="/dashboard/attendees">Attendees</Link>
         </div>
-        <div className="hover:bg-gray-200">
+        <div className="hover:bg-gray-50 rounded p-2 outline outline-1 outline-slate-100">
           <Link className="w-full font-bold py-2 p-2 mb-auto rounded" href="/dashboard/">Settings</Link>
         </div>
         <div className="hidden h-auto w-full grow rounded-md md:block"></div> 
       </div>
 
       {/* sign out */}
-      <div className="rounded bg-gray-100 mr-1 ml-1 shadow">
-        <button className="bg-base-100 hover:bg-base-200 font-bold py-2 px-2">
+      <div className="rounded mr-1 ml-1 outline outline-1 outline-slate-100">
+        <button className="font-bold py-2 px-2 p-2">
           <Link href="/">Sign out</Link>
         </button>
       </div>
