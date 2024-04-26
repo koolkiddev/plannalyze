2680e67429666e7b623ff048890adc00c637fd5d
koolkiddev <koolfacts67@gmail.com>
2024-04-26 16:59:39 -0500

    tables

--- src/app/dashboard/attendees/page.tsx
+++ src/app/dashboard/attendees/page.tsx
@@ -1,30 +1,18 @@
-export default function Page() {
-    return (
-        <table className="table-auto">
-            <thead>
-                <tr>
-                    <th>Song</th>
-                    <th>Artist</th>
-                    <th>Year</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    <td>Malcolm Lockyer</td>
-                    <td>1961</td>
-                </tr>
-                <tr>
-                    <td>Witchy Woman</td>
-                    <td>The Eagles</td>
-                    <td>1972</td>
-                </tr>
-                <tr>
-                    <td>Shining Star</td>
-                    <td>Earth, Wind, and Fire</td>
-                    <td>1975</td>
-                </tr>
-            </tbody>
-        </table>
-    );
+import { sql } from "@vercel/postgres";
+export default async function Cart({
+  params
+} : {
+  params: { user: string }
+}): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * from ATTENDEES`;
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.name}
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/app/ui/dashboard/side-nav.tsx
+++ src/app/ui/dashboard/side-nav.tsx
@@ -3,9 +3,9 @@ import { karla } from '../fonts';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2 outline outline-2 outline-slate-100">
+    <div className="flex h-full flex-col px-3 py-4 md:px-2 outline outline-2 outline-slate-100 shadow-md">
       {/* title */}
-      <div className="rounded bg-gray-100 mb-4 mr-1 ml-1">
+      <div className="rounded bg-gray-100 mb-4 mr-1 ml-1 shadow">
         <Link
           className="mb-2 flex h-20 items-end justify-start p-4 md:h-40"
           href="/"
@@ -19,7 +19,7 @@ export default function SideNav() {
       </div> 
 
       {/* nav links */}
-      <div className="flex grow flex-row p-2 mb-4 mr-1 ml-1 justify-between space-x-4 md:flex-col md:space-x-0 md:space-y-4 rounded bg-gray-100">
+      <div className="flex grow flex-row p-2 mb-4 mr-1 ml-1 justify-between space-x-4 md:flex-col md:space-x-0 md:space-y-4 rounded bg-gray-100 shadow">
         <div className="hover:bg-gray-200">
           <Link className="w-full font-bold py-2 p-2 mb-auto rounded-xl" href="/dashboard/">Home</Link>
         </div>
@@ -33,7 +33,7 @@ export default function SideNav() {
       </div>
 
       {/* sign out */}
-      <div className="rounded bg-gray-100 mr-1 ml-1">
+      <div className="rounded bg-gray-100 mr-1 ml-1 shadow">
         <button className="bg-base-100 hover:bg-base-200 font-bold py-2 px-2">
           <Link href="/">Sign out</Link>
         </button>
